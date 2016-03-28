5cad45758a5ecf1598bd440be7e002ba5bb12cc9
kenjiro kariya <kandroid51@gmail.com>
2016-03-28 21:20:28 +0900

    add mediacontroller to control a media file. modify layout width of VideoView.

--- myplayvideo/src/main/java/com/example/ken26/myplayvideo/PlayVideo.java
+++ myplayvideo/src/main/java/com/example/ken26/myplayvideo/PlayVideo.java
@@ -6,26 +6,27 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class PlayVideo extends AppCompatActivity {
-
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_video);
 
-        button = (Button) findViewById(R.id.button);
         getWindow().setFormat(PixelFormat.UNKNOWN);
         Intent intent = getIntent();
+
         Uri uri = Uri.parse(intent.getStringExtra("VideoFile"));
         VideoView mVideoPlay = (VideoView) findViewById(R.id.VideoView);
+        MediaController mediaController = new MediaController(this);
 
         mVideoPlay.setVideoURI(uri);
         mVideoPlay.requestFocus();
         mVideoPlay.start();
+        mVideoPlay.setMediaController(mediaController);
 
 
     }
